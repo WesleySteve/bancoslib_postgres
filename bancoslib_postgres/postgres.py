b317859599052046ce73cd14d6ba07964f06fef2
Wesley Steve <wesley.silva23@gotmail.com>
2022-01-15 13:35:59 -0300

    feat: adicionado (connect_db_postgres)

---
+++ bancoslib_postgres/postgres.py
@@ -0,0 +1,38 @@
+import os
+
+import dotenv
+import sqlalchemy
+
+
+
+def connect_db_postgres(dotenv_path=os.path.expanduser("~/.env")):
+    """Função para se conectar no banco de dados
+
+    Args:
+        dotenv_path (caminho das variaveis de ambiente, optional):
+        pesquisa .env iniciando na raiz do usuario caso ele não passe um caminho.
+        caminho padrão: os.path.expanduser("~/.env").
+
+    Returns:
+        conexao: retorna a conexao com o banco de dados postgres
+    """
+
+    # carregando o dotenv
+    dotenv.load_dotenv(dotenv_path)
+
+    # importando as variaveis de ambiente
+    _host = os.getenv("POSTGRES_HOST")
+    _port = os.getenv("POSTGRES_PORT")
+    _user = os.getenv("POSTGRES_USER")
+    _paswd = os.getenv("POSTGRES_PASSWORD")
+    _db = os.getenv("POSTGRES_DB")
+    
+    
+    # definindo string de conexao com o banco de dados postgres
+    str_connection = f"postgresql+pypostgresql://{_user}:{_paswd}@{_host}:{_port}/{_db}"
+
+    # criando a conexao com o banco de dados postgres
+    con = sqlalchemy.create_engine(str_connection)
+    
+    
+    return con
